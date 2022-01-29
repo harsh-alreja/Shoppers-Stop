2a33ca008d314d75bffdf0b57f2e2ebc56b0ccd0
harsh-alreja <56589430+harsh-alreja@users.noreply.github.com>
2022-01-29 13:12:51 +0530

    user authentication and registeration backend

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,37 +1,11 @@
 import express from 'express';
-import asyncHandler from 'express-async-handler';
-import Product from '../models/productModel.js';
+import {
+  getProductById,
+  getProducts,
+} from '../controllers/productController.js';
 const router = express.Router();
 
-//@desc Fetch all products
-//@routes GET /api/products
-//@access Public
-
-router.get(
-  '/',
-  asyncHandler(async (req, res) => {
-    const products = await Product.find({});
-    // throw new Error('Some Error in Product.Routes')
-    res.json(products);
-  })
-);
-
-//@desc Fetch single products
-//@routes GET /api/products/:id
-//@access Public
-
-router.get(
-  '/:id',
-  asyncHandler(async (req, res) => {
-    // const product = products.find((p) => p._id === req.params.id )
-    const product = await Product.findById(req.params.id);
-    if (product) {
-      res.json(product);
-    } else {
-      res.status(404);
-      throw new Error('Product not found');
-    }
-  })
-);
+router.route('/').get(getProducts);
+router.route('/:id').get(getProductById);
 
 export default router;
--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ import dotenv from 'dotenv';
 import Colors from 'colors';
 import { notFound,errorHandler } from './middleware/errorMiddleware.js';
 import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
+
 
 const PORT = process.env.PORT || 5000;
 
@@ -13,11 +15,15 @@ connectDB();
 
 const app = express();
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
   res.send('API is running !');
 });
 
 app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
+
 
 
 app.use(notFound)
